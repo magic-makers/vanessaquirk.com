25074e403bc44ef5b0931033b0aa9272e00b9489
Zev Goldstein <zev.goldstein@gmail.com>
2020-12-12 16:27:01 -0500

    add placeholder for missing images

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -2,11 +2,20 @@ import React from 'react';
 import Img from 'gatsby-image';
 
 export default ({ article, className }) => {
+  const renderImg = (img) => {
+    if (img) {
+      return <Img alt="" sizes={img.sizes} />;
+    }
+    // placeholder white square
+    return (
+      <svg>
+        <rect width="100%" height="100%" fill="white" />
+      </svg>
+    );
+  };
   return (
     <div className={'tile ' + className}>
-      <span className="image">
-        <Img alt="" sizes={article.image.sizes} />
-      </span>
+      <span className="image">{renderImg(article.image)}</span>
       <a target="_blank" rel="noopener noreferrer" href={article.url}>
         <h2>{article.title}</h2>
       </a>
