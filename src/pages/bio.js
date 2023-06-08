d3c2311f189aa566922780ebff260c96efe28e98
vmquirk <54515679+vmquirk@users.noreply.github.com>
2023-06-08 12:09:30 -0500

    Update bio.js

--- src/pages/bio.js
+++ src/pages/bio.js
@@ -19,25 +19,25 @@ const BioPage = () => (
         </p>
         <p>
           You may have heard me as the co-host on a few podcasts:{' '}
-          <a href="https://sidewalklabs.com/podcast/">City of the Future</a>,
-          where fellow cities journalist{' '}
-          <a href="http://www.eric-jaffe.com/home">Eric Jaffe</a> and I explore
-          the ideas and innovations that will transform cities; the{' '}
-          <a href="https://sidewalklabs.com/podcast/">Sidewalk Weekly</a>, where
-          Eric and I cover the week’s top stories in urban tech;{' '}
+         <a href="https://urbanistmedia.org/about-the-podcast/">Urban Roots</a>, where historic preservationist Deqah
+          Hussein-Wetzel and I preserve the stories of places significant to 
+          people of color in story; {' '}
           <a href="https://uncertain.substack.com/">Uncertain Things</a>, where
           my journalist roommate Adaam James Levin-Areddy and I talk to the
           thinkers who are navigating us through this current, chaotic moment;
-          and  <a href="https://urbanistmedia.org/about-urban-roots/">Urban Roots</a>, where historic preservationist Deqah
-          Hussein-Wetzel and I tell the untold stories of remarkable people and
-          places in urban history.
+        <a href="https://sidewalklabs.com/podcast/">City of the Future</a>,
+          where fellow cities journalist{' '}
+          <a href="http://www.eric-jaffe.com/home">Eric Jaffe</a> and I explored
+          the ideas and innovations that will transform cities; and the{' '}
+          <a href="https://sidewalklabs.com/podcast/">Sidewalk Weekly</a>, where
+          Eric and I covered the week’s top stories in urban tech.
         </p>
         <p>
           Born in Spain to a Northern Irish father and a Peruvian mother, I
           begrudgingly grew up on Long Island, pursued English literature and
           Music at the George Washington University in Washington D.C. and
-          Oxford University in the U.K., left the northern hemisphere to pasarlo
-          chancho in Chile, and then moved back to New York in 2014 to pursue an
+          Oxford University in the U.K., left the northern hemisphere to volunteer
+          teach English in Chile, and then moved back to New York in 2014 to pursue an
           M.S. in Arts & Culture Journalism from Columbia University. I
           currently call Queens home. And when I’m not making podcasts, I’m
           probably{' '}
